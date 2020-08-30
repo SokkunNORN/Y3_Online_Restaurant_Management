faea990cad91602364cdabb6230d2b20c7dd1ffc
sokkunnorn <sokkun.norn2019@gmail.com>
2020-08-30 11:20:27 +0700

    add template page menu

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/menu', function () {
+    return view('menu');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
