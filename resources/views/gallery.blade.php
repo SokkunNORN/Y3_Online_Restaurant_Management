3ab505d5cbec8b0cc65931188bb4bc5f2206f767
sokkunnorn <sokkun.norn2019@gmail.com>
2020-08-30 15:28:43 +0700

    update auth

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -4,37 +4,6 @@
 
 @section('content')	
 
-	<div id="page">
-		<nav class="fh5co-nav" role="navigation">
-			<!-- <div class="top-menu"> -->
-				<div class="container">
-					<div class="row">
-						<div class="col-xs-12 text-center logo-wrap">
-							<div id="fh5co-logo"><a href="index.html">Tasty<span>.</span></a></div>
-						</div>
-						<div class="col-xs-12 text-center menu-1 menu-wrap">
-							<ul>
-								<li><a href="index.html">Home</a></li>
-								<li><a href="menu.html">Menu</a></li>
-								<li class="has-dropdown active">
-									<a href="gallery.html">Gallery</a>
-									<ul class="dropdown">
-										<li><a href="#">Events</a></li>
-										<li><a href="#">Food</a></li>
-										<li><a href="#">Coffees</a></li>
-									</ul>
-								</li>
-								<li><a href="reservation.html">Reservation</a></li>
-								<li><a href="about.html">About</a></li>
-								<li><a href="contact.html">Contact</a></li>
-							</ul>
-						</div>
-					</div>
-					
-				</div>
-			<!-- </div> -->
-		</nav>
-
 		<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" style="background-image: url(images/hero_1.jpeg);" data-stellar-background-ratio="0.5">
 			<div class="overlay"></div>
 			<div class="container">
@@ -152,67 +121,6 @@
 				</div>
 			</div>
 		</div>
-
-		<footer id="fh5co-footer" role="contentinfo" class="fh5co-section">
-			<div class="container">
-				<div class="row row-pb-md">
-					<div class="col-md-4 fh5co-widget">
-						<h4>Tasty</h4>
-						<p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
-					</div>
-					<div class="col-md-2 col-md-push-1 fh5co-widget">
-						<h4>Links</h4>
-						<ul class="fh5co-footer-links">
-							<li><a href="#">Home</a></li>
-							<li><a href="#">About</a></li>
-							<li><a href="#">Menu</a></li>
-							<li><a href="#">Gallery</a></li>
-						</ul>
-					</div>
-
-					<div class="col-md-2 col-md-push-1 fh5co-widget">
-						<h4>Categories</h4>
-						<ul class="fh5co-footer-links">
-							<li><a href="#">Landing Page</a></li>
-							<li><a href="#">Real Estate</a></li>
-							<li><a href="#">Personal</a></li>
-							<li><a href="#">Business</a></li>
-							<li><a href="#">e-Commerce</a></li>
-						</ul>
-					</div>
-
-					<div class="col-md-4 col-md-push-1 fh5co-widget">
-						<h4>Contact Information</h4>
-						<ul class="fh5co-footer-links">
-							<li>198 West 21th Street, <br> Suite 721 New York NY 10016</li>
-							<li><a href="tel://1234567920">+ 1235 2355 98</a></li>
-							<li><a href="mailto:info@yoursite.com">info@yoursite.com</a></li>
-							<li><a href="http://https://freehtml5.co">freehtml5.co</a></li>
-						</ul>
-					</div>
-
-				</div>
-
-				<div class="row copyright">
-					<div class="col-md-12 text-center">
-						<p>
-							<small class="block">&copy; 2016 Free HTML5. All Rights Reserved.</small> 
-							<small class="block">Designed by <a href="http://freehtml5.co/" target="_blank">FreeHTML5.co</a> Demo Images: <a href="http://unsplash.co/" target="_blank">Unsplash</a></small>
-						</p>
-						<p>
-							<ul class="fh5co-social-icons">
-								<li><a href="#"><i class="icon-twitter2"></i></a></li>
-								<li><a href="#"><i class="icon-facebook2"></i></a></li>
-								<li><a href="#"><i class="icon-linkedin2"></i></a></li>
-								<li><a href="#"><i class="icon-dribbble2"></i></a></li>
-							</ul>
-						</p>
-					</div>
-				</div>
-
-			</div>
-		</footer>
-	</div>
 
 @endsection
 
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,23 +2,49 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12 text-center logo-wrap">
-                <div id="fh5co-logo"><a href="index.html">Tasty<span>.</span></a></div>
+                <div id="fh5co-logo"><a href="/home">Online Restaurant Management<span>.</span></a></div>
             </div>
             <div class="col-xs-12 text-center menu-1 menu-wrap">
                 <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="menu.html">Menu</a></li>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/menu">Menu</a></li>
                     <li class="has-dropdown">
-                        <a href="gallery.html">Gallery</a>
+                        <a href="/gallery">Gallery</a>
                         <ul class="dropdown">
                             <li><a href="#">Events</a></li>
                             <li><a href="#">Food</a></li>
                             <li><a href="#">Coffees</a></li>
                         </ul>
                     </li>
-                    <li><a href="reservation.html">Reservation</a></li>
-                    <li><a href="about.html">About</a></li>
-                    <li class="active"><a href="contact.html">Contact</a></li>
+                    <li><a href="/reservation">Reservation</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li class="active"><a href="/contact">Contact</a></li>
+
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="has-dropdown">
+                            <a href="gallery">{{ Auth::user()->name }}</a>
+                            <ul class="dropdown">
+                                <li><a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a></li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </ul>
+                        </li>
+                    @endguest
+
                 </ul>
             </div>
         </div>
